42b023f8b036b8b97f83f8ee251aeae1bea2bdbe
Joshua Villamarzo <jvillamarzo@gmail.com>
2012-06-18 11:53:40 -0700

    Added timeout polling to buildhook

--- gitosis/buildhook.py
+++ gitosis/buildhook.py
@@ -9,20 +9,52 @@ socket_location = config.get('Gitosis Config', 'socket')
 context = zmq.Context()
 print "Connecting to server..."
 socket = context.socket(zmq.REQ)
-socket.connect (socket_location)
+socket.setsockopt(zmq.LINGER, 0)
+socket.connect(socket_location)
+
+# use poll for timeouts:
+poller = zmq.Poller()
+poller.register(socket, zmq.POLLIN)
+timeout = 3*1000 # 3s timeout in milliseconds
+
+def polltest():
+    #socket.send_json({"msg": "testmsg"}) # send can block on other socket types, so keep track
+    socket.send("Hello")
+   
+    if poller.poll(timeout):
+        #msg = socket.recv_json()
+        message = socket.recv()
+        print "Received reply [", message, "]"
+    else:
+        #raise IOError("Timeout processing auth request")
+        print "Couldn't Connect to Server, please try again"
+
+    # these are not necessary, but still good practice:
+    #socket.close()
+    #context.term()
+
+    #sys.exit(0)
 
 def test():
     #  Do 10 requests, waiting each time for a response
-    for request in range (1,10):
+    for request in range(1,10):
         print "Sending request ", request,"..."
-        socket.send ("Hello")
+        socket.send("Hello")
         #  Get the reply.
-        message = socket.recv()
-        print "Received reply ", request, "[", message, "]"
+        if poller.poll(timeout):
+            message = socket.recv()
+            print "Received reply ", request, "[", message, "]"
+        else:
+            print "Couldn't Connect to Server, please try again"
 
 def notify(reponame):
     print "Sending request for repo '"+reponame+"' to build server"
     socket.send(reponame)
-    # Get the reply.
-    message = socket.recv()
-    print "Received reply [", message, "]"
+
+    if poller.poll(timeout):
+        # Get the reply
+        message = socket.recv()
+        print "Received reply [", message, "]"
+    else:
+        print "Couldn't Connect to Server, please try again"
+
